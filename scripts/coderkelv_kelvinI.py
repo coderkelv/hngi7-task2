f541f9b127b5db5dda258ca337206a564d0121ac
coderkelv <65327684+coderkelv@users.noreply.github.com>
2020-06-03 22:34:37 +0100

    Add files via upload

---
+++ scripts/coderkelv_kelvinI.py
@@ -0,0 +1,6 @@
+full_name = ('Kelvin Iseh')
+id = ('HNG-00076')
+language = ('Python')
+
+print('Hello World, this is ' + full_name + ' with HNGi7 ID ' +
+      id + ' using ' + language + ' for stage 2 task.')
