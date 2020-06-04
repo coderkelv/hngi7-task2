590d9ae64cb41edca351079a69aca3fd75b1d394
coderkelv <65327684+coderkelv@users.noreply.github.com>
2020-06-04 22:19:39 +0100

    updated email

--- scripts/coderkelv_kelvinI.py
+++ scripts/coderkelv_kelvinI.py
@@ -1,6 +1,7 @@
-full_name = ('Kelvin Iseh')
+name = ('Kelvin Iseh')
 hng_id = ('HNG-00076')
 language = ('Python')
+email = ('iseh25@gmail.com')
 
-print('Hello World, this is ' + full_name + ' with HNGi7 ID ' +
-      id + ' using ' + language + ' for stage 2 task.')
+
+print('Hello World, this is ' + name + ' with HNGi7 ID ' + hng_id + ' using ' + language + ' for stage 2 task email ' + email)
